0ee2e49911de7dd4756868d0e68ac7e31e4116e2
Gabriel Moreira <gabrielmscampos@gmail.com>
2021-11-03 11:53:00 -0300

    first commit

---
+++ btageffanalyzer/__init__.py
@@ -0,0 +1,156 @@
+#!/usr/bin/env python
+
+import json
+import pkgutil
+
+import numpy as np
+
+btagging_file = pkgutil.get_data(__package__, "data/btagging.json").decode("utf-8")
+btagging = json.loads(btagging_file)
+del btagging_file
+
+hadron_flavour_file = pkgutil.get_data(__package__, "data/hadron_flavour.json").decode(
+    "utf-8"
+)
+hadron_flavour = json.loads(hadron_flavour_file)
+del hadron_flavour_file
+
+
+class BTaggingEfficiencyMap:
+    """
+    Class for computing b-tagging efficiency map for a given dataset.
+    """
+
+    def __init__(self, df, eta_bins):
+        self.df = df
+        self.eta_bins = eta_bins
+
+    def calib(self, year, algo, working_point):
+        """
+        Calibrate dataset for b-tagging algorithm working point.
+
+        Args:
+            year (str): Monte Carlo campaign year - 2016, 2017, 2018
+            algo (str): b-tagging algorithm - DeepCSV, DeepJet
+            working_point (str): b-tagging algorithm working point - loose, medium, tight
+        """
+        btag_thr = btagging.get(year).get(algo).get(working_point)
+        if algo.lower() == "deepcsv":
+            df_tagged = self.df[self.df.Jet_btagDeepB > btag_thr]
+        elif algo.lower() == "deepjet":
+            df_tagged = self.df[self.df.Jet_btagDeepFlavB > btag_thr]
+        else:
+            raise ValueError("b-tagging algorithm must be deepcsv or deepjet.")
+
+        self.df_tagged = df_tagged.reset_index(drop=True)
+
+    def compute_pt_bins(self, pt_min, pt_max, step_size, max_unc):
+        """
+        Compute pt bins sweeping pt in range [pt_min, pt_max] by given step size,
+        a given bin is accepted if the statistical uncertainty in the number of b-tagged events
+        is positive and lesser then `unc` for all eta_bins.
+
+        Args:
+            pt_min (list, np.array): Minimum pt for sweeping.
+            pt_max (list, np.array): Maximum pt for sweeping.
+            step_size (float, int): Sweeping step size in pt array.
+            max_unc (float, int): Accepted statistical uncertainty in the number of b-tagged events.
+        """
+        pt_bins = [pt_min]
+        current_bin = (pt_min, pt_min + step_size)
+
+        while current_bin[1] < pt_max:
+            multiplier = 0
+            hist_tag, _, _ = np.histogram2d(
+                self.df_hf_tagged.Jet_pt,
+                np.abs(self.df_hf_tagged.Jet_eta),
+                bins=[pt_bins, self.eta_bins],
+                weights=self.df_hf_tagged.evtWeight,
+            )
+            hist_tag2, _, _ = np.histogram2d(
+                self.df_hf_tagged.Jet_pt,
+                np.abs(self.df_hf_tagged.Jet_eta),
+                bins=[pt_bins, self.eta_bins],
+                weights=self.df_hf_tagged.evtWeight ** 2,
+            )
+            unc = np.sqrt(hist_tag2) / hist_tag
+            if (unc < max_unc).all() and (unc > 0).all():
+                multiplier = 1
+                pt_bins.append(current_bin[1])
+                current_bin = (current_bin[1], current_bin[1] + step_size)
+            else:
+                multiplier += 1
+                current_bin = (current_bin[0], current_bin[1] + multiplier * step_size)
+                if current_bin[1] + multiplier * step_size >= pt_max:
+                    if len(pt_bins) > 1:
+                        del pt_bins[-1]
+                    pt_bins.append(current_bin[1] + multiplier * step_size)
+                    break
+
+        return pt_bins
+
+    def make(self, pt_min=None, pt_max=None, step_size=None, max_unc=None):
+        """
+        Make b-tagging efficiency map for given dataset
+        """
+        efficiency_map = {hf: [] for hf in hadron_flavour.keys()}
+        uncertainty_map = {hf: [] for hf in hadron_flavour.keys()}
+
+        if max_unc is None:
+            raise ValueError("max_argument must be an dict or a float.")
+
+        for hf_value, hf_id in hadron_flavour.items():
+
+            # Filter by hadron flavour
+            df_flav = self.df[self.df.Jet_hadronFlavour == hf_id]
+            df_flav = df_flav.reset_index(drop=True)
+            df_btag = self.df_tagged[self.df_tagged.Jet_hadronFlavour == hf_id]
+            df_btag = df_btag.reset_index(drop=True)
+
+            # Compute pt bins
+            if isinstance(max_unc, int):
+                munc = max_unc
+            else:
+                munc = max_unc.get(hf_value)
+
+            self.df_hf_tagged = df_btag.copy()
+            pt_bins = []
+            while len(pt_bins) <= 2:
+                pt_bins = self.compute_pt_bins(pt_min, pt_max, step_size, munc)
+                if munc >= 0.5:
+                    break
+                munc += 0.001
+
+            # Compute efficiency histogram
+            hist_flav, xedges_flav, yedges_flav = np.histogram2d(
+                df_flav.Jet_pt,
+                np.abs(df_flav.Jet_eta),
+                bins=[pt_bins, self.eta_bins],
+                weights=df_flav.evtWeight,
+            )
+            hist_btag, _, _ = np.histogram2d(
+                df_btag.Jet_pt,
+                np.abs(df_btag.Jet_eta),
+                bins=[pt_bins, self.eta_bins],
+                weights=df_btag.evtWeight,
+            )
+
+            effm = []
+            for i in range(len(xedges_flav) - 1):
+                for j in range(len(yedges_flav) - 1):
+                    eff = hist_btag[i, j] / hist_flav[i, j]
+                    if np.isnan(eff):
+                        eff = None
+                    effm.append(
+                        {
+                            "eta_min": yedges_flav[j],
+                            "eta_max": yedges_flav[j + 1],
+                            "pt_min": xedges_flav[i],
+                            "pt_max": xedges_flav[i + 1],
+                            "eff": hist_btag[i, j] / hist_flav[i, j],
+                        }
+                    )
+            efficiency_map[hf_value] = effm
+            uncertainty_map[hf_value] = munc
+
+        return efficiency_map, uncertainty_map
